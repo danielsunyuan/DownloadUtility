94a89744d3f744c1cd13aee4dcb9cf6bcb880eba
Daniel <danielsunyuan@gmail.com>
2023-10-12 23:03:19 +1100

    added arguments

--- AudioUtility/downloadAudio.py
+++ AudioUtility/downloadAudio.py
@@ -1,42 +1,53 @@
 import subprocess
-import os
 
 class LinkToAudioDownloader:
-    def __init__(self, links_file, download_dir, bash_script_path):
-        self.links_file = links_file
+    def __init__(self, download_dir, bash_script_path, link=None):
+        self.links_file = "audio_links.txt"
         self.download_dir = download_dir
         self.bash_script_path = bash_script_path
-
-    def download_audio(self):
         
+        if link is not None:
+            self.links = [link]
+        else:
+            self.links = []
+
+    def links_from_file(self):
         # Read audio links from the external file
         with open(self.links_file, "r") as file:
-            links = file.read().splitlines()
+            self.links = file.read().splitlines()
 
+
+    def bash_shell(self):
         # Open the Bash script for writing
         with open(self.bash_script_path, "w") as bash_script:
             bash_script.write("#!/bin/bash\n")
             bash_script.write("source activate utility\n")
 
             # Write each audio link to the script and specify the download directory
-            for link in links:
+            for link in self.links:
                 download_command = f"yt-dlp '{link}' -x --audio-format wav -o '{self.download_dir}/%(title)s.%(ext)s'\n"
                 bash_script.write(download_command)
 
         # Make and Run the Bash script executable
         subprocess.run(["chmod", "+x", self.bash_script_path])
         subprocess.run(["./" + self.bash_script_path])
-        
+
         # Clear the bash script
         with open(self.bash_script_path, "w"):
             pass
 
-if __name__ == "__main__":
+    def download_audio(self):
+        # If no argument is passed into the class.. then read from the file
+        if not self.links:
+            self.links_from_file()
+        self.bash_shell()
 
+if __name__ == "__main__":
     downloader = LinkToAudioDownloader(
-        links_file="links.txt",
+        # Flume - 71m3
+        link="https://www.youtube.com/watch?v=9DJS_aNGxHI",
         download_dir="downloaded_audio",
         bash_script_path="commands.sh"
-        )
+    )
     
     downloader.download_audio()
--- VideoUtility/downloadVideo.py
+++ VideoUtility/downloadVideo.py
@@ -2,16 +2,22 @@ import subprocess
 import os
 
 class VideoDownloader:
-    def __init__(self, links_file="video_links.txt", download_dir="downloaded_videos", bash_script_path="commands.sh"):
-        self.links_file = links_file
+    def __init__(self, link=None, download_dir="downloaded_videos", bash_script_path="commands.sh"):
+        self.links_filepath = "video_links.txt"
         self.download_dir = download_dir
         self.bash_script_path = bash_script_path
 
-    def download_videos(self):
-        # Read video links from the external file
-        with open(self.links_file, "r") as file:
-            video_links = file.read().splitlines()
+        if link is not None:
+            self.links = [link]
+        else:
+            self.links = []
 
+    def links_from_file(self):
+        # Read video links from the external file
+        with open(self.links_filepath, "r") as file:
+            self.links = file.read().splitlines()
+
+    def bash_shell(self):
         # Open the Bash script for writing
         with open(self.bash_script_path, "w") as bash_script:
             # Write the Conda activation command to the script
@@ -22,13 +28,19 @@ class VideoDownloader:
             os.makedirs(self.download_dir, exist_ok=True)
 
             # Write each video link to the script and specify the download directory
-            for link in video_links:
+            for link in self.links:
                 download_command = f"yt-dlp '{link}' --recode-video mp4 -o '{self.download_dir}/%(title)s.%(ext)s'\n"
                 bash_script.write(download_command)
 
         # Make and run the Bash script executable
         subprocess.run(["chmod", "+x", self.bash_script_path])
         subprocess.run(["./" + self.bash_script_path])
+
+    def download_videos(self):
+        # If no argument is passed into the class.. then read from the file
+        if not self.links:
+            self.links_from_file()
+        self.bash_shell()
 
 if __name__ == "__main__":
     # Create an instance of the VideoDownloader class
